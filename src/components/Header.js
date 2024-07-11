cee317f0d9ee9ba7a5f72a584d48a12e2e31ea57
carlos <carlmaundu@gmail.com>
2024-07-11 14:39:57 +0300

    Header Styling and Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Header.css';
+import logo from '../img/logo.jpg';
 
 const Header = () => {
     return (
-        <header className="bg-dark text-white py-3">
+        <header className="header bg-dark text-white py-3">
             <div className="container d-flex justify-content-between align-items-center">
                 <div className="d-flex align-items-center">
-                    {/* <imgx
-                        src="../../public/9483173.png"
+                <img
+                        src={logo}
                         alt="Logo"
                         className="mr-2"
                         style={{ width: '50px' }}
-                    /> */}
+                    />
                     <h1 className="h4 m-0">Infrastructure Calculator</h1>
                 </div>
                 <Link to="/calculator" className="btn btn-primary">Try it out</Link>
